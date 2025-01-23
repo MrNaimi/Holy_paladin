98b5b619441d7da67b906fbb0ce01c0dd694d6dc
tuulosss <samu@tuulos.net>
2025-01-23 19:40:10 +0200

    hitbox delay lisätty
    hitbox delay lisätty

--- holy_paladin/enemy.gd
+++ holy_paladin/enemy.gd
@@ -5,7 +5,6 @@ var speed = 30
 func _physics_process(delta: float) -> void:
 	#var direction = (target.position-position).normalized()
 	#velocity = direction*speed
-	look_at(target.position)
 	move_and_slide()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
--- holy_paladin/light_attack_1.gd
+++ holy_paladin/light_attack_1.gd
@@ -2,9 +2,13 @@ extends Area2D
 
 @export var damage = 1
 @export var hitboxtime = 0.5
+@export var hitboxdelay = 0.2
 @onready var hit_box_timer: Timer = $HitBoxTimer
 @onready var hitbox: CollisionShape2D = $CollisionShape2D
 @onready var player_animations: AnimatedSprite2D = $"../../Player_animations"
+@onready var hit_box_start_timer: Timer = $HitBoxStartTimer
+
+
 @onready var original_position = hitbox.position.x
 @onready var original_rotation = hitbox.rotation_degrees
 
@@ -24,7 +28,11 @@ func _on_hit_box_timer_timeout() -> void:
 
 #Tässä määritetään funktio joka enabloi hitboxin ja aloittaa sille ajastimen
 func enableHitBox() -> void:
-
-	hitbox.disabled = false
+	hit_box_start_timer.wait_time = hitboxdelay
+	hit_box_start_timer.start()
 	hit_box_timer.wait_time = hitboxtime
 	hit_box_timer.start()
+
+
+func _on_hit_box_start_timer_timeout() -> void:
+	hitbox.disabled = false
