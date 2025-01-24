ae5edb9b579a7915d7331086cc3d72f6aa9e4803
tuulosss <samu@tuulos.net>
2025-01-24 10:36:54 +0200

    line of sight testailua

--- holy_paladin/enemy.gd
+++ holy_paladin/enemy.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
+
 @onready var target: CharacterBody2D = $"../Player"
 
-
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 var speed = 30
 func _physics_process(delta: float) -> void:
-	var direction = (target.position-position).normalized()
-	velocity = direction*speed
-	#look_at(target.position)
-	move_and_slide()
+	ray_cast_2d.set_target_position(target.position-position)
+	print(ray_cast_2d.get_collider())
+	if ray_cast_2d.get_collider() is not TileMapLayer:
+		var direction = (target.position-position).normalized()
+		velocity = direction*speed
+		move_and_slide()
+	else:
+		pass
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("area entered")
