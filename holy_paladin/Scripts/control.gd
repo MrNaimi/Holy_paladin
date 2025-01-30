75cdd2da8a6a2903cfae8b6b0ae8c6938a94146b
tuulosss <samu@tuulos.net>
2025-01-30 16:41:27 +0200

    UI säätelyä
    UI säätelyä

--- holy_paladin/Scripts/control.gd
+++ holy_paladin/Scripts/control.gd
@@ -6,27 +6,44 @@ extends Control
 @onready var viewportsize: Label = $VBoxContainer/viewportsize
 @onready var characterspeed: Label = $VBoxContainer/characterspeed
 @onready var combotimer: Label = $VBoxContainer/combotimer
-@onready var combo_timer: Timer = $"../../ComboTimer"
+@onready var combo_timer: Timer = $"../../Node2D/Player/ComboTimer"
+@onready var player: CharacterBody2D = $"../../Node2D/Player"
+
+
+@onready var xp_bar: TextureProgressBar = $Control/XPbar
+@onready var hp_bar: TextureProgressBar = $Control/HPbar
+
+
 @onready var pieru: AudioStreamPlayer2D = $"../../../pieru"
 @onready var pierucounter: Label = $VBoxContainer/pierucounter
 @onready var processpeed: Label = $VBoxContainer/processpeed
+@onready var combotimerbar: TextureProgressBar = $Control/combotimerbar
 
-@onready var player: CharacterBody2D = $"../.."
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
+	xp_bar.value+=50*delta
+	if xp_bar.value >= 100:
+		xp_bar.value = 0
+	hp_bar.value-=30*delta
+	if hp_bar.value <= 0:
+		hp_bar.value = 100
+	combotimerbar.value=combo_timer.time_left
+	
+		
+		
 	x.text ="mouse pos x: "+str(get_viewport().get_mouse_position().x)
 	y.text ="mouse pos y: "+str(get_viewport().get_mouse_position().y)
-	character_x.text = "character pos x: "+str(player.position.x)
+	character_x.text = "charactaser pos x: "+str(player.position.x)
 	character_y.text = "character pos y: "+str(player.position.y)
 	viewportsize.text = "viewport size: "+str(get_viewport().size.x)
 	characterspeed.text = "character speed: "+str(player.current_speed)
 	combotimer.text = "combo time left: "+str(float(combo_timer.time_left)).left(4)
 	pierucounter.text = str(player.pierucounter)
 	processpeed.text = str(player.processiterations)
+	
