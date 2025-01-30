59d0fa544df1ac469e7b54c4d62a131b6ddd8760
Iiro Pääkkönen <iiro.paakkonen@gmail.com>
2025-01-30 19:14:19 +0200

    dash toimii

--- holy_paladin/Scripts/character_body_2d.gd
+++ holy_paladin/Scripts/character_body_2d.gd
@@ -7,10 +7,12 @@ extends CharacterBody2D
 var pierucounter = 0
 var combo = 1
 var processiterations = 0
+var dashing
 @export var SPEED = 100.0
 @onready var hp = 10
 
 #Viittaukset player attackkeihin
+@onready var animation_timer: Timer = $AnimationTimer
 
 @onready var light_attack_1: Area2D = $AttackHitboxes/Light_attack1
 @onready var light_attack_2: Area2D = $AttackHitboxes/Light_attack2
@@ -26,22 +28,43 @@ func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
 	
-	#IF statements to flip sprite and play the walking animation
 	if get_viewport().get_mouse_position().x >= get_viewport().size.x/2 and input_direction.x != 0:
-		player_animations.flip_h = false;
-		player_animations.play("walk")
+		if !dashing:
+			player_animations.play("walk")
+		else:
+			pass
 	elif get_viewport().get_mouse_position().x <= get_viewport().size.x/2 and input_direction.x != 0:
-		player_animations.flip_h = true;
-		player_animations.play("walk")
+		if !dashing:
+			player_animations.play("walk")
+		else:
+			pass
 	if input_direction.y != 0:
-		player_animations.play("walk")
+		if !dashing:
+			player_animations.play("walk")
+		else:
+			pass
 	if input_direction.x == 0 && input_direction.y == 0:
 		if !player_animations.is_playing():
 			player_animations.play("idle")
 			
-		
 #checkaa mouse inputin kun clickaa. toistaa atm vaan animaation kerran
 func _input(event):
+	if event.is_action_pressed("dash") and velocity.x != 0:
+		print("dash pressed")
+		if velocity.x > 0:
+			player_animations.flip_h = false
+		elif velocity.x < 0:
+			player_animations.flip_h = true
+		player_animations.play("dash")
+		if !player_animations.flip_h:
+			get_tree().create_tween().tween_property(self, "position", Vector2(position.x+tween_direction*120, position.y),0.5)
+			tween_direction = 1
+		elif player_animations.flip_h:
+			get_tree().create_tween().tween_property(self, "position", Vector2(position.x-tween_direction*120, position.y),0.5)
+			tween_direction = 1
+		animation_timer.start()
+		dashing = true
+			
 	if current_speed==0:
 		if event.is_action_pressed("attack"):
 			print("mouse clicked")
@@ -104,3 +127,7 @@ func _on_combo_timer_timeout() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_animation_timer_timeout() -> void:
+	dashing = false
