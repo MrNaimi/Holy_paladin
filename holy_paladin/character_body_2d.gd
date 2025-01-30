43c0d3cd0c1a45a6dfe793d2ca5915b80389b5b3
tuulosss <samu@tuulos.net>
2025-01-30 10:32:52 +0200

    root node muutettu, tween testailua
    root node muutettu, tween testailua

--- holy_paladin/character_body_2d.gd
+++ holy_paladin/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var player_animations: AnimatedSprite2D = $Player_animations
 @onready var combo_timer: Timer = $ComboTimer
 @onready var pieru: AudioStreamPlayer2D = $"../pieru"
+@onready var player: CharacterBody2D = $"."
 var pierucounter = 0
 var combo = 1
 var processiterations = 0
@@ -13,11 +14,12 @@ var processiterations = 0
 @onready var light_attack_1: Area2D = $AttackHitboxes/Light_attack1
 @onready var light_attack_2: Area2D = $AttackHitboxes/Light_attack2
 @onready var light_attack_3: Area2D = $AttackHitboxes/Light_attack3
-
+@onready var tween = get_tree().create_tween()
 
 var current_speed = 0
 func _ready():
 	print("pylly")
+	
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * SPEED
@@ -38,6 +40,7 @@ func get_input():
 		
 #checkaa mouse inputin kun clickaa. toistaa atm vaan animaation kerran
 func _input(event):
+	var tween_direction = 1
 	if current_speed==0:
 		if event.is_action_pressed("attack"):
 			print("mouse clicked")
@@ -48,11 +51,15 @@ func _input(event):
 			if combo == 1 and player_animations.animation == "idle":
 				combo_timer.stop()
 				player_animations.play("light_attack1")
+				if player_animations.flip_h:
+					tween_direction = -1
+				get_tree().create_tween().tween_property(self,"position",Vector2(position.x+tween_direction*100,position.y),0.2)
 				light_attack_1.enableHitBox()
 				print("attack1 played")
 				combo_timer.start()
 				combo = 2
 				print("timer started")
+				
 			elif combo == 2 and player_animations.animation == "idle":
 				combo_timer.stop()
 				player_animations.play("light_attack2")
@@ -66,6 +73,7 @@ func _input(event):
 				light_attack_3.enableHitBox()
 				print("attack3 played")
 				combo = 1
+				
 func _physics_process(delta: float) -> void:
 	get_input()
 	move_and_slide()
