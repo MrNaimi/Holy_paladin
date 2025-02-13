8efb8fd81a94ecdfda5e53a07c79b8376315257b
MrNaimi <antti.niemi4@hotmail.com>
2025-02-13 17:37:52 +0200

    Pari uutta vastustajaa ja spawnit kuntoon

--- holy_paladin/Scenes/evil_wizard.gd
+++ holy_paladin/Scenes/evil_wizard.gd
@@ -4,73 +4,69 @@ extends CharacterBody2D
 var copy = false
 var returning = false
 var og_position
-var speed = 95.0
+var speed = 75.0
 var player_chase = false
 var player = null
 @export var health = 3
 @export var damage = 1
 var idle = ""
-@onready var wolf_animation: AnimatedSprite2D = $wolf_animation
+@onready var wizard_animation: AnimatedSprite2D = $wizard_animation
 var colour = ""
 var rng = RandomNumberGenerator.new()
 
 func _ready():
 	og_position = position
-	#Annetaan susille numero 1 ja 4 väliltä jonka avulla valitaan suden animaation värit
-	var colour_num = rng.randi_range(1,4)
-	if colour_num == 1:
-		colour = "grey"
-	elif colour_num == 2:
-		colour = "brown"
-	elif colour_num == 3:
-		colour = "mushroom"
-	else:
-		colour = "white"
-	print("Wolves colour is " + colour)
-
+	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
-	if is_instance_valid(wolf_animation):
-		if !wolf_animation.is_playing():
-			wolf_animation.play(colour + "_idle")
+	if is_instance_valid(wizard_animation):
+		if !wizard_animation.is_playing():
+			wizard_animation.play( "idle")
 		#Kääntää suden idle animaation suunnan 1/1500 todennäköisyydellä per frami
-		if RandomNumberGenerator.new().randi_range(0, 10000)==9 && wolf_animation.animation == colour + "_idle":
-			print("Wolf has turned around")
-			if wolf_animation.flip_h:
-				wolf_animation.flip_h = false
+		if RandomNumberGenerator.new().randi_range(0, 10000)==9 && wizard_animation.animation == "idle":
+			print("Wizard has turned around")
+			if wizard_animation.flip_h:
+				wizard_animation.flip_h = true
 			else:
-				wolf_animation.flip_h = true
+				wizard_animation.flip_h = false
 		#Pistää suden jahtaamaan pelaajaa
-		if player_chase && !wolf_animation.animation == (colour + "_hit") && !wolf_animation.animation == (colour + "_death") && !wolf_animation.animation == (colour + "_attack"):
-			wolf_animation.play(colour + "_run")
+		if player_chase and !wizard_animation.animation in ["hit", "death"]:
+			if wizard_animation.animation != "attack":
+				wizard_animation.play("run")
 			velocity = Vector2(1, 0)
 			if player.position[0] > position[0]:
-				wolf_animation.flip_h = true
+				wizard_animation.flip_h = false
 			else:
-				wolf_animation.flip_h = false
+				wizard_animation.flip_h = true
 			var direction = ((player.position-Vector2(0, 20)) - position).normalized()
 			position += direction * speed * delta
 			
 			# Hyökkää pelaajaan jos etäisyys on alle 25
-			if position.distance_to(player.position)<25:
-				print("Wolf attacked player")
-				wolf_animation.play(colour + "_attack")
-		
+			if position.distance_to(player.position)<60:
+				print("Wizard attacked player")
+				wizard_animation.play("attack")
+				direction = ((player.position-Vector2(0, 20)) - position).normalized()
+				speed = 30
+
+			if position.distance_to(player.position)>100:
+				wizard_animation.play("run")
+				speed = 75
+			
 		#Palauttaa suden takaisin alkuperäisellä paikalleen
 		elif returning:
 			if og_position[0] > position[0]:
-				wolf_animation.flip_h = true
+				wizard_animation.flip_h = false
 			else:
-				wolf_animation.flip_h = false
+				wizard_animation.flip_h = true
 			var direction = (og_position - position).normalized()
 			position += direction * speed * delta
 			
 			#if velocity == Vector2(0,0):
-				#wolf_animation.play(colour + "_idle")
+				#wizard_animation.play("idle")
 				
 			if position.distance_to(og_position)<2:
-				wolf_animation.play(colour + "_idle")
+				wizard_animation.play("idle")
 				returning = false
 
 #Antaa signaalin, että on aika lähteä jahtaamaan pelaajaa
@@ -83,14 +79,15 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 #Susi ottaa damagea kun siihen lyödään ja kuolee kun healtti menee nollaan.
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("attack"):
-		print("Wolf has taken damage")
+		print("Wizard has taken damage")
 		health -= area.damage
 		if health > 0:
-			wolf_animation.play(colour + "_hit")
+			wizard_animation.play("hit")
+			speed = 75
 		if health <= 0:
-			print("Wolf has died")
+			print("Wizard has died")
 			GlobalVariables.xp += 1
-			wolf_animation.play(colour + "_death")
+			wizard_animation.play("death")
 			await get_tree().create_timer(1).timeout
 			wolves.queue_free()
 	if area.is_in_group("player"):
@@ -101,4 +98,4 @@ func _on_aggro_off_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		returning = true
 		player_chase = false
-		wolf_animation.play(colour + "_run")
+		wizard_animation.play("run")
